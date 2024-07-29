43ed670c7440b0ddc67904f17d922d09b535f763
adithyako <38380981+adithyako@users.noreply.github.com>
2024-07-29 11:16:32 -0400

    added popup

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,7 +1,7 @@
 import { oauth2 as SMART } from 'fhirclient';
 import React from 'react';
 import './SignIn.css'; // Adjust the path as necessary
-import signInLogo from '/Users/sooryarajendran/fhir-react/src/EPIC HYPERSPACE.png'; // Adjust path as necessary
+import signInLogo from './EPIC HYPERSPACE.png'; // Adjust path as necessary
 
 const SignIn = () => {
     const clientId = "9e43034e-949f-41f5-880e-eb31a7663bee"; // Replace with your client id
